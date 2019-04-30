88c5dd4057d32b22c4efa44eb1c231fe0664fb0e
dit08 <dit@imac-3.local>
2019-04-30 15:18:51 +0900

    테이블뷰 만들고 동물 리스트 만들기

--- simpleTableView/ViewController.swift
+++ simpleTableView/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tv: UITableView!
+    
+    var animals = ["cat","dog","pig","bird","whale","cow"]
+    var detail = ["고양이","강아지","돼지","새","고래","황소"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tv.dataSource = self
     }
-
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return animals.count
+    }
+    
+    //cell 만듦
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        //cell 재활용
+        let cell = tv.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = animals[row]
+        cell.detailTextLabel?.text = detail[row]
+        
+        cell.imageView?.image = UIImage(named: animals[row])
+        
+        return cell
+    }
 
 }
 
