e0518877e4d0cd1b39b82f744d66b9295f7cbc16
dit08 <dit@imac-3.local>
2019-05-07 15:55:59 +0900

    심플테이블뷰에 화면전환 첨가

---
+++ simpleTableView/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  simpleTableView
+//
+//  Created by dit08 on 2019. 5. 7..
+//  Copyright © 2019년 dit. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: NSObject {
+
+}
